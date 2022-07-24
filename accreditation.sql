60987bd9335009554a0501752be3fa71261f59f5
marialumayor <mariakristinalumayor@gmail.com>
2022-07-24 22:08:31 +0800

    SQL

---
+++ accreditation.sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 06, 2022 at 03:46 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `accreditation`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `AccountID` int(11) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Role` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`AccountID`, `Name`, `Role`) VALUES
+(1, 'Maria Kristina Lumayor', 'Faculty');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `colleges`
+--
+
+CREATE TABLE `colleges` (
+  `CollegeCode` varchar(10) NOT NULL,
+  `CollegeName` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `colleges`
+--
+
+INSERT INTO `colleges` (`CollegeCode`, `CollegeName`) VALUES
+('CABEIHM', 'College of Accountancy, Business, Economics, and International Hospitality Management'),
+('CAS', 'College of Arts and Sciences'),
+('CE', 'College of Engineering'),
+('CICS', 'College of Informatics and Computing Sciences'),
+('CIT', 'College of Industrial Technology'),
+('CONAHS', 'College of Nursing and Allied Health Sciences'),
+('CTE', 'College of Teacher Education');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `credentials`
+--
+
+CREATE TABLE `credentials` (
+  `CredID` int(11) NOT NULL,
+  `AccountID` int(11) NOT NULL,
+  `Username` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `credentials`
+--
+
+INSERT INTO `credentials` (`CredID`, `AccountID`, `Username`, `Password`) VALUES
+(1, 1, 'maria', '263bce650e68ab4e23f28263760b9fa5');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`AccountID`);
+
+--
+-- Indexes for table `colleges`
+--
+ALTER TABLE `colleges`
+  ADD PRIMARY KEY (`CollegeCode`);
+
+--
+-- Indexes for table `credentials`
+--
+ALTER TABLE `credentials`
+  ADD PRIMARY KEY (`CredID`),
+  ADD KEY `AccountID` (`AccountID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `AccountID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `credentials`
+--
+ALTER TABLE `credentials`
+  MODIFY `CredID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `credentials`
+--
+ALTER TABLE `credentials`
+  ADD CONSTRAINT `credentials_ibfk_1` FOREIGN KEY (`AccountID`) REFERENCES `accounts` (`AccountID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
